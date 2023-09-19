7e3d9912b052a47f9b5b9a6bae0e4509d645a251
Luca Iacovelli <liaco1306@gmail.com>
2023-09-19 12:20:57 -0400

    pop up sorta

--- app/src/main/java/com/example/attendancetracker/MainActivity.java
+++ app/src/main/java/com/example/attendancetracker/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -98,13 +102,25 @@ public class MainActivity extends AppCompatActivity {
 
         buttonEnter.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.confirmationpopup, null);
 
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+                popupWindow.setElevation(20);
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
             }
         });
 
 
-        getData();
-        addItemToSheet();
+        //getData();
+        //addItemToSheet();
     }
 
     private void getData() {
